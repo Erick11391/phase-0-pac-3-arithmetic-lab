cb7b4428ae7af636962ec34e88fa549a77fd0d1a
Erick11391 <xandererik4@gmail.com>
2024-11-23 23:01:23 +0300

    new added file

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a ,b) {
+    return a + b;
+
+}
+function subtract(a ,b) {
+    return a - b;
+}
+function multiply(a ,b) {
+    return a * b
+}
+function divide(a ,b) {
+    return a / b
+}
+
+function add5() {
+    return (number += 5);
+  }
+  
+  function subtract8() {
+    return (number -= 3);
+  }
+  // Increment function
+function increment(n) {
+    return n + 1;
+  }
+  
+  // Decrement function
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // makeInt function
+  function makeInt(n) {
+    return parseInt(n, 10); // Ensures base 10 is used
+  }
+  
+  // preserveDecimal function
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
+  
